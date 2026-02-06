a4c659fdbc218be145daa65205ab5d82bb9d55de
zekedavidson <bhatiagary28@gmail.com>
2026-02-06 18:52:32 +0530

    fixed images

--- src/components/sections/FAQSection.tsx
+++ src/components/sections/FAQSection.tsx
@@ -32,9 +32,9 @@ const FAQSection = () => {
 
     return (
         <section className='w-full bg-[#FBF6F1] py-16 md:py-24'>
-            <div className='grid grid-cols-1 md:grid-cols-[1.5fr_2fr] gap-15 items-start max-w-[1600px] mx-auto px-6 md:px-12 lg:px-16 2xl:px-16 3xl:px-20'>
+            <div className='grid grid-cols-1 md:grid-cols-[1.5fr_2fr] gap-40 items-start max-w-[1500px] mx-auto px-6 md:px-12 lg:px-16 2xl:px-20'>
                 {/* Left: Flower Image */}
-                <div className='relative h-[400px] md:h-[500px] lg:h-[700px] 2xl:h-[750px] 3xl:h-[900px] order-1'>
+                <div className='relative h-[400px] md:h-[500px] lg:h-[700px] 2xl:h-[780px] order-1'>
                     <div className='absolute inset-0 rounded-t-full overflow-hidden'>
                         <Image
                             src="/faq-section.jpg"
@@ -46,8 +46,8 @@ const FAQSection = () => {
                 </div>
 
                 {/* Right: FAQs */}
-                <div className='flex flex-col gap-8 order-2 '>
-                    <h2 className='text-3xl md:text-4xl lg:text-5xl 2xl:text-5xl 3xl:text-6xl font-medium text-[#223614] mb-10 mt-32 2xl:mt-40 3xl:mt-50'>
+                <div className='flex flex-col gap-7 order-2 '>
+                    <h2 className='text-3xl md:text-4xl lg:text-5xl 2xl:text-6xl font-medium text-[#223614] mb-10 mt-32 2xl:mt-40 3xl:mt-50'>
                         FAQs
                     </h2>
 
@@ -57,13 +57,13 @@ const FAQSection = () => {
                             <div key={index} className='border-t border-b border-[#223614]'>
                                 <button
                                     onClick={() => toggleFAQ(index)}
-                                    className='w-full py-6 flex gap-4 items-center text-left hover:opacity-70 transition-opacity'
+                                    className='w-full py-4 flex gap-4 items-center text-left hover:opacity-70 transition-opacity'
                                 >
                                     <span className='text-2xl md:text-5xl font-normal text-[#223614] shrink-0'>
                                         {openIndex === index ? '−' : '+'}
                                     </span>
 
-                                    <span className='text-xl md:text-2xl lg:text-5xl font-medium text-[#223614] pr-4'>
+                                    <span className='text-xl md:text-2xl lg:text-5xl font-medium text-[#223614]'>
                                         {faq.question}
                                     </span>
 
